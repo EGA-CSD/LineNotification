12eabdfc7b0af0ea79414671661849dac8ce2751
Prakarn Sirima <prakarn.sirima@ega.or.th>
2017-09-06 16:43:18 +0700

    test 1st commit and push

---
+++ MailGoThai/ega.Consoles.Test/Class1ZimbraTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Security.Cryptography;
+
+//is method what call for create account in zimbra
+public class test
+{
+    public static void CreateNewZimbraAccount(string username, string domainName, string firstName, string lastName)
+    {
+        SoapParameters myParam = new SoapParameters()
+    {
+    
+    accountUsername = string.Format("{0}@{1}", username, domainName),
+    accountFirstName = firstName,
+    accountLastName = lastName,
+    };
+    AccountService.Create(myParam);
+    }
+
+    // class AccountService
+
+    public static void Create(SoapParameters param)
+    {
+    GetAdministratorAuthenticationToken(param);
+    CreateAccountRequest(param);
+    }
+
+    private static void GetAdministratorAuthenticationToken(SoapParameters param)
+    {
+    XmlDocument doc = GetResponseDoc(param, SoapMessages.AuthenticationToken);
+    param.authToken = doc.GetElementsByTagName("authToken").Item(0).InnerXml;
+    }
+
+    private static XmlDocument GetResponseDoc(SoapParameters param, SoapMessages message)
+    {
+    string soapMessage = SoapMessageBuilder(message, param);
+    XmlDocument requestDoc = new XmlDocument();
+    requestDoc.LoadXml(soapMessage);
+    HttpWebRequest request = HttpWebRequestBuilder(param.serviceAdminSoapUri);
+    Stream stm = request.GetRequestStream();
+    requestDoc.Save(stm);
+    stm.Close();
+    WebResponse resp = request.GetResponse(); // here occurs error ---// Unable to //establish a relationship of trust secure channel for SSL / TLS, .
+    stm = resp.GetResponseStream();
+    StreamReader r = new StreamReader(stm);
+    XmlDocument responseDoc = new XmlDocument();
+    responseDoc.LoadXml(r.ReadToEnd());
+    return responseDoc;
+    }
+
+    private static HttpWebRequest HttpWebRequestBuilder(string serviceAdminSoapUri)
+    {
+    HttpWebRequest retVal = (HttpWebRequest)WebRequest.Create(serviceAdminSoapUri);
+    retVal.Headers.Add("SOAPAction", "''");
+    retVal.ContentType = "text/xml;charset='utf - 8 '";
+    retVal.Accept = "text/xml";
+    retVal.Method = "POST";
+    return retVal;
+    }
+
+    private static string SoapMessageBuilder(SoapMessages message, SoapParameters param){
+    string retVal = "";
+    switch(message)
+    {
+    case SoapMessages.AuthenticationToken :
+        
+        retVal = "<?xml version=\"1.0\" encoding=\"utf-8\" ?> ";
+        retVal += "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\>";
+        retVal += " <soap:Header> ";
+        retVal += "<context xmlns=\"urn:zimbra\"> ";
+        retVal += "<format type=\"xml\"/> ";
+        retVal += "</context> ";
+        retVal += "</soap:Header> ";
+        retVal += "<soap:Body> ";
+        retVal += "<AuthRequest xmlns=\"urn:zimbraAdmin\"> ";
+        retVal += "<account by=\"name\">cuenta@</account> ";
+        retVal += "<password>Clave segura</password> ";
+        retVal += "</AuthRequest> ";
+        retVal += "</soap:Body> ";
+        retVal += "</soap:Envelope>" ;
+    break;
+    return retVal;
+    }
+    }
+    public  SoapParameters()
+    {
+    string impersonateDomain = System.Configuration.ConfigurationSettings.AppSettings["Domain.Settings.ImpersonateDomain"];
+    string impersonateUsername = System.Configuration.ConfigurationSettings.AppSettings["Domain.Settings.ImpersonateUsername"];
+    string impersonatePassword = System.Configuration.ConfigurationSettings.AppSettings["Domain.Settings.ImpersonatePassword"];
+    string zimbraServiceAdminSoapUri = System.Configuration.ConfigurationSettings.AppSettings["Domain.Settings.ZimbraServiceAdminSoapUri"];
+    adminUsername = string.Format("{0}@{1}", impersonateUsername, impersonateDomain);
+    adminPassword = impersonatePassword;
+    serviceAdminSoapUri = zimbraServiceAdminSoapUri;
+    }
+
+    private static void CreateAccountRequest(SoapParameters param)
+    {
+    XmlDocument doc = GetResponseDoc(param, SoapMessages.CreateAccount);
+    try
+    {
+    string value = doc.GetElementsByTagName("CreateAccountResponse").Item(0).ChildNodes[0].Attributes["id"].Value;
+    Guid guid = new Guid(value);
+    param.accountId = guid.ToString();
+    }
+    catch
+    {
+    throw new Exception("Account Did Not Appear To Be Created.");
+    }
+    }
+    private enum SoapMessages
+    {
+    AuthenticationToken,
+    CreateAccount,
+    RemoveAccount,
+    Account
+    }
+
+}
---
+++ MailGoThai/ega.Consoles.Test/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+
+using System.Xml;
+
+namespace ega.Consoles.Test
+{
+    class Program : ICertificatePolicy
+    {
+
+        public bool CheckValidationResult(ServicePoint srvPoint,
+        X509Certificate certificate, WebRequest request,
+        int certificateProblem)
+        {
+            //Return True to force the certificate to be accepted.
+            return true;
+        }
+
+        static void SendNotification(string argMsg)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://notify-api.line.me/api/notify");
+            var postData = string.Format("message={0}", argMsg);
+            var data = Encoding.UTF8.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+            request.Headers.Add("Authorization", "Bearer TRp6byyCsJG7S2poh5ON3zdH88SSm3LMffZ1fXy8o1H"); //KlPhgOKMqBYSuLsZBLAY7uUCXD1s0jEjwHfbUPbQE0I
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            Console.WriteLine("\r\n{0} \r\n",responseString);
+        }
+        static void Main(string[] args)
+        {
+            string stoken = string.Empty;
+            System.Net.ServicePointManager.CertificatePolicy = new Program();
+            StringBuilder xml = new StringBuilder();
+            xml.Append("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+            xml.Append("<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">");
+            xml.Append("<soap:Header>");
+            xml.Append("<context xmlns=\"urn:zimbra\">");
+            xml.Append("<format type=\"xml\"/>");
+            xml.Append("</context>");
+            xml.Append("</soap:Header>");
+            xml.Append("<soap:Body>");
+            xml.Append("<AuthRequest xmlns=\"urn:zimbraAccount\">");
+            xml.Append("<account by=\"name\">mailchecker@training.mail.go.th</account>");    //@@ thaiairways_admin@api.mail.go.th
+            xml.Append("<password>Ega@2017</password>");                                      //@@ QR1kcD3
+            xml.Append("</AuthRequest>");
+            xml.Append("</soap:Body>");
+            xml.Append("</soap:Envelope>");
+            Console.WriteLine(xml);
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create("https://accounts.mail.go.th/service/soap");
+            httpRequest.ContentType = "application/soapxml";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(xml.ToString());
+
+            httpRequest.Method = "POST";
+            httpRequest.ContentLength = byteArray.Length;
+            try
+            {
+                using (var stream = httpRequest.GetRequestStream())
+                {
+                    stream.Write(byteArray, 0, byteArray.Length);
+                }
+                var response = (HttpWebResponse)httpRequest.GetResponse();
+
+                var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(responseString);
+
+                //Console.ReadLine();
+
+                XmlDocument responseDoc = new XmlDocument();
+                responseDoc.LoadXml(responseString);
+                string authtoken = responseDoc.GetElementsByTagName("authToken").Item(0).InnerXml;
+                stoken = authtoken;
+                Console.WriteLine("\r\nauthToken:{0}\r\n", stoken);
+                // retPreAuthTokenValue = authtoken;
+                if (!string.IsNullOrEmpty(stoken))
+                {
+                    Console.WriteLine("not need to send notification..");
+                    SendNotification("[MailGoThai]: สามารถเชื่อมต่อ MailGoThai ได้ (เทสโดย : mailchecker@training.mail.go.th)");
+                }
+                else
+                {
+                    SendNotification("[MailGoThai]: ไม่สามารถเก็ทค่า token ได้");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("error: {0}",ex.Message);
+                Console.WriteLine("send notification to LINE..");
+                SendNotification("[MailGoThai]: ไม่สามารถเชื่อมต่อ MailGoThai ได้");
+            }
+        }
+    }
+}
